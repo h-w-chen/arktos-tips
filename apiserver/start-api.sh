ffc7abd3ec6a13d46c3c5ba21304c0aac0fd065b
Hongwei Chen <hong.chen@futurewei.com>
2020-06-16 09:45:09 -0700

    tried http; unable to go arounf SA etc

--- apiserver/start-api.sh
+++ apiserver/start-api.sh
@@ -1,1 +1,4 @@
-sudo ./kube-apiserver --etcd-servers=http://127.0.0.1:2379 --admission-control=""
+sudo ./kube-apiserver --etcd-servers=http://127.0.0.1:2379 --admission-control="" 
+
+// this does not work with http 
+sudo ./kube-apiserver --etcd-servers=http://127.0.0.1:2379 --admission-control="ServiceAccount" --allowed-privileged=true
---
+++ controller-manager/start-controller-manager.sh
@@ -0,0 +1,1 @@
+--allocate-node-cidrs=true --cluster-cidr="192.168.0.0/16"
